018d262a22c12f1d56b3ce28c05ba0b084dcf29f
lesterbv06 <bloodshot_x@hotmail.com>
2022-06-07 12:43:19 -0500

    styled landing page

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyledHeader, Nav, Logo, Image } from './styles/Header.styled';
+import { Container } from './styles/Container.styled';
+import { Flex } from './styles/Flex.styled';
+import { Button } from './styles/Button.styled';
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <Container>
+        <Nav>
+          <Logo src='./images/logo.svg' alt='Logo' />
+          <Button>Try It Free</Button>
+        </Nav>
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+            <Button bg='#ff0099' color='fff'>
+              Get Started for Free
+            </Button>
+          </div>
+          <Image src='./images/illustration-mockups.svg' alt='' />
+        </Flex>
+      </Container>
+    </StyledHeader>
+  );
+}
